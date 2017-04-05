c1b64062a00e0255fbd5dce058ad0da60ff840df
alsayannyi <al.sayannyi@gmail.com>
2017-04-05 19:02:19 +0300

    image optimization menu was modified isotope styles were changed added home-button

--- dessert/js/main.js
+++ dessert/js/main.js
@@ -199,9 +199,9 @@ $(document).ready(function(){
 // });
 
 
-$(document).ready(function(){
-	$('.flip').hover(function(){
-        $(this).find('.card').toggleClass('flipped');
-    });
-});
+// $(document).ready(function(){
+// 	$('.flip').hover(function(){
+//         $(this).find('.card').toggleClass('flipped');
+//     });
+// });
 
